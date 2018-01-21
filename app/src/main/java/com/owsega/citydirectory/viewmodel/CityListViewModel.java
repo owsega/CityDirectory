8339be360acb22642627bbf62e196a2c0b9d89ee
seyi <owoeye.g.o@gmail.com>
2018-01-21 10:21:50 +0100

    remove android specific imports in viewModel
    This allows for unit testing  ... without having to use an instrumentation test
    It also follows proper viewmodel architectural pattern

--- app/src/main/java/com/owsega/citydirectory/viewmodel/CityListViewModel.java
+++ app/src/main/java/com/owsega/citydirectory/viewmodel/CityListViewModel.java
@@ -5,8 +5,6 @@ import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 import android.arch.paging.LivePagedListBuilder;
 import android.arch.paging.PagedList;
-import android.os.AsyncTask;
-import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -64,14 +62,15 @@ public class CityListViewModel extends ViewModel implements OnCityClickListener 
     }
 
     private void getAllCities(JsonReader reader) {
-        AsyncTask.execute(() -> {
-            Log.d(TAG, "starting getAllcities");
+        Executors.newSingleThreadExecutor().execute(() -> {
+//        AsyncTask.execute(() -> {
+            System.out.println(TAG + " starting getAllcities");
             long time = System.currentTimeMillis();
             Type type = new TypeToken<List<City>>() {
             }.getType();
             List<City> cities = new Gson().fromJson(reader, type);
             time = System.currentTimeMillis() - time;
-            Log.d(TAG, "time to parse json " + time);
+            System.out.println(TAG + " time to parse json " + time);
             ConcurrentNavigableMap<String, City> citiesMap = new ConcurrentSkipListMap<>();
             for (City city : cities) {
                 citiesMap.put(city.getKey(), city);
@@ -120,14 +119,13 @@ public class CityListViewModel extends ViewModel implements OnCityClickListener 
 
     private void filterWithMap(String text) {
         String lowerBound = fullData.ceilingKey(text);
-        Log.d(TAG, "full Data size " + fullData.size());
 
         int len = text.length() - 1;
         String higherBound = text.substring(0, len) + (char) (text.charAt(len) + 1);
         higherBound = fullData.ceilingKey(higherBound);
 
         if (lowerBound == null || higherBound == null) {
-            Log.d(TAG, "null bound");
+            System.out.println(TAG + " null bound");
             emptyData.postValue(true);
         } else {
             try {
@@ -139,7 +137,7 @@ public class CityListViewModel extends ViewModel implements OnCityClickListener 
                 if (newMap.size() < 1) emptyData.postValue(true);
                 else setList(newMap);
             } catch (Exception e) {
-                Log.d(TAG, "error creating filtered map " + e.getMessage());
+                System.out.println(TAG + " error creating filtered map " + e.getMessage());
                 e.printStackTrace();
                 emptyData.postValue(true);
             }
