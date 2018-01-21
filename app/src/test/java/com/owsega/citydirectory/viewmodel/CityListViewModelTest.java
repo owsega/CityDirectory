d47cf59d18d0efb809f45a817b54b26a54cc275e
seyi <owoeye.g.o@gmail.com>
2018-01-21 22:29:11 +0100

    test: add tests for city clicks and data reload
    these tests are for ensuring that the data is not being reloaded
    at every init call (that is, everytime the activity is reloaded due to
    keyboard or orientation changes or whatnot)

--- app/src/test/java/com/owsega/citydirectory/viewmodel/CityListViewModelTest.java
+++ app/src/test/java/com/owsega/citydirectory/viewmodel/CityListViewModelTest.java
@@ -15,13 +15,16 @@ import org.mockito.ArgumentCaptor;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 public class CityListViewModelTest {
 
@@ -96,6 +99,62 @@ public class CityListViewModelTest {
         verify(emptyDataObserver, atLeastOnce()).onChanged(false);
         verify(listObserver, atLeastOnce()).onChanged(captor.capture());
         assertThat(4, CoreMatchers.equalTo(captor.getValue().size()));
+
+        // test empty query
+        viewModel.filterCities("");
+        verify(emptyDataObserver, atLeastOnce()).onChanged(false);
+        verify(listObserver, atLeastOnce()).onChanged(captor.capture());
+        assertThat(TEST_JSON_SIZE, CoreMatchers.equalTo(captor.getValue().size()));
     }
 
+    @Test
+    public void testOnCityClicked() {
+        ArgumentCaptor<PagedList<City>> captor = forClass(PagedList.class);
+        Observer<PagedList<City>> observer = mock(Observer.class);
+        viewModel.init(jsonReader, false);
+        viewModel.cityList.observeForever(observer);
+        verify(observer).onChanged(captor.capture());
+
+        City city = captor.getValue().get(0);
+        Observer<City> cityObserver = mock(Observer.class);
+        viewModel.selectedCity.observeForever(cityObserver);
+        viewModel.onCityClicked(city);
+        verify(cityObserver, atLeastOnce()).onChanged(city);
+    }
+
+    @Test
+    public void testDataInBackground() throws UnsupportedEncodingException {
+        Observer<Boolean> observer = mock(Observer.class);
+        viewModel.dataReady.observeForever(observer);
+        viewModel.init(jsonReader, true);
+        verifyNoMoreInteractions(observer);
+    }
+
+    @Test
+    public void testDataRecreation() throws UnsupportedEncodingException {
+        try {
+            viewModel = new CityListViewModel();
+            Observer<Boolean> observer = mock(Observer.class);
+            viewModel.dataReady.observeForever(observer);
+
+            // test dataReady not observed at first init call (because it's in background thread)
+            InputStream in = getClass().getClassLoader().getResourceAsStream(TEST_JSON);
+            jsonReader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+            viewModel.init(jsonReader, true);
+            verifyNoMoreInteractions(observer);
+
+            // test dataReady is observed on second call on the main thread, even though data
+            // is loaded on a background thread. This means data is not being reloaded
+            // every time init is called, but only the first time
+            in = getClass().getClassLoader().getResourceAsStream(TEST_JSON);
+            jsonReader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+            viewModel.init(jsonReader, true);
+            verify(observer).onChanged(any());
+        } catch (RuntimeException rte) {
+            // may throw exception if data loading is completed before the test is finished
+            // this is because Android Looper is not mocked, and executing data loading on
+            // the background thread will post to android's main thread at completion.
+            rte.printStackTrace();
+        }
+    }
 }
